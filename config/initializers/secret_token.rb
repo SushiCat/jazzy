df49627729de5ef89711b4f3f66129270eddd52e
SushiCat <t3h.rouge@gmail.com>
2011-11-19 04:38:22 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jazzy::Application.config.secret_token = 'c577d317ecfcc18cf52d2a8697d47859eecd6fd1dfc7f86a37fa8f59b100dd8b931e8f9b61f5fce32c9af8c84ca1c04f03214e4ab39efee27a2dbc750c5eb53f'
